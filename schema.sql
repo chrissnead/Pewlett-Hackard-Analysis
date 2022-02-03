bcd976ae2c7947beea7fb34b70c7c2bc9669bc90
Christopher Snead <christophersnead@Christophers-MacBook-Pro.local>
2022-02-02 21:06:14 -0800

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,29 +1,29 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+  dept_no VARCHAR(4) NOT NULL,
+  dept_name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (dept_no),
+  UNIQUE (dept_name)
 );
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
+  birth_date DATE NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  gender VARCHAR NOT NULL,
+  hire_date DATE NOT NULL,
+  PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -37,11 +37,11 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-dept_no VARCHAR(6) NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+  dept_no VARCHAR(6) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles (
@@ -52,5 +52,3 @@ CREATE TABLE titles (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no, title, from_date)
 );
-
-SELECT * FROM departments;
