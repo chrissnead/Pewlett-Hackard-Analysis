de065ba59cdd6a84297472ad988d987da53d8ab2
Christopher Snead <christophersnead@Christophers-MacBook-Pro.local>
2022-02-02 20:52:18 -0800

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -17,12 +17,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -35,14 +35,13 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_employee (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR(6) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles (
@@ -51,5 +50,5 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, from_date)
 );
