e5d94260c4075735ec6ed9cc2969463c2d64a802
Christopher Snead <christophersnead@Christophers-MacBook-Pro.local>
2022-02-04 21:57:48 -0800

    added 3 csv files to Data folder

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,6 +1,10 @@
 -- Deliverable 1
-SELECT e.emp_no, e.first_name, e.last_name,
-	t.title, t.from_date, t.to_date
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	t.title, 
+	t.from_date, 
+	t.to_date
 INTO retirement_titles
 FROM employees AS e
 JOIN titles AS t
@@ -9,3 +13,44 @@ WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
 SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- Number of employees retiring by job title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date, 
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
